4320308d23a5b22fb6280692f3d42eaa159968b5
Tomoyuki Fujimori <moyu@dromozoa.com>
2018-06-03 13:29:41 +0900

    tuple3を追加。

---
+++ dromozoa/vecmath/tuple3.lua
@@ -0,0 +1,259 @@
+-- Copyright (C) 2018 Tomoyuki Fujimori <moyu@dromozoa.com>
+--
+-- This file is part of dromozoa-vecmath.
+--
+-- dromozoa-vecmath is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- dromozoa-vecmath is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with dromozoa-vecmath.  If not, see <http://www.gnu.org/licenses/>.
+
+local rawset = rawset
+
+local function set(a, x, y, z)
+  if x then
+    if y then
+      a[1] = x
+      a[2] = y
+      a[3] = z
+    else
+      a[1] = x[1]
+      a[2] = x[2]
+      a[3] = x[3]
+    end
+  else
+    a[1] = 0
+    a[2] = 0
+    a[3] = 0
+  end
+  return a
+end
+
+local function equals(a, b)
+  return #a == #b and a[1] == b[1] and a[2] == b[2] and a[3] == b[3]
+end
+
+local class = {
+  set = set;
+  equals = equals;
+}
+local metatable = { __eq = equals }
+
+function class.get(a, b)
+  b[1] = a[1]
+  b[2] = a[2]
+  b[3] = a[3]
+  return a
+end
+
+function class.add(a, b, c)
+  if c then
+    a[1] = b[1] + c[1]
+    a[2] = b[2] + c[2]
+    a[3] = b[3] + c[3]
+  else
+    a[1] = a[1] + b[1]
+    a[2] = a[2] + b[2]
+    a[3] = a[3] + b[3]
+  end
+  return a
+end
+
+function class.sub(a, b, c)
+  if c then
+    a[1] = b[1] - c[1]
+    a[2] = b[2] - c[2]
+    a[3] = b[3] - c[3]
+  else
+    a[1] = a[1] - b[1]
+    a[2] = a[2] - b[2]
+    a[3] = a[3] - b[3]
+  end
+  return a
+end
+
+function class.negate(a, b)
+  if b then
+    a[1] = -b[1]
+    a[2] = -b[2]
+    a[3] = -b[3]
+  else
+    a[1] = -a[1]
+    a[2] = -a[2]
+    a[3] = -a[3]
+  end
+  return a
+end
+
+function class.scale(a, s, b)
+  if b then
+    a[1] = s * b[1]
+    a[2] = s * b[2]
+    a[3] = s * b[3]
+  else
+    a[1] = s * a[1]
+    a[2] = s * a[2]
+    a[3] = s * a[3]
+  end
+  return a
+end
+
+function class.scale_add(a, s, b, c)
+  if c then
+    a[1] = s * b[1] + c[1]
+    a[2] = s * b[2] + c[2]
+    a[3] = s * b[3] + c[3]
+  else
+    a[1] = s * a[1] + b[1]
+    a[2] = s * a[2] + b[2]
+    a[3] = s * a[3] + b[3]
+  end
+  return a
+end
+
+function class.epsilon_equals(a, b, epsilon)
+  local x = a[1] - b[1]
+  if x < 0 then
+    if epsilon < -x then return false end
+  else
+    if epsilon < x then return false end
+  end
+
+  local y = a[2] - b[2]
+  if y < 0 then
+    if epsilon < -y then return false end
+  else
+    if epsilon < y then return false end
+  end
+
+  local z = a[3] - b[3]
+  if z < 0 then
+    if epsilon < -z then return false end
+  else
+    if epsilon < z then return false end
+  end
+
+  return true
+end
+
+function class.clamp(a, min, max, b)
+  if b then
+    local x = b[1]
+    local y = b[2]
+    local z = b[3]
+    if x < min then a[1] = min elseif x > max then a[1] = max else a[1] = x end
+    if y < min then a[2] = min elseif y > max then a[2] = max else a[2] = y end
+    if z < min then a[3] = min elseif z > max then a[3] = max else a[3] = z end
+  else
+    local x = a[1]
+    local y = a[2]
+    local z = a[3]
+    if x < min then a[1] = min elseif x > max then a[1] = max end
+    if y < min then a[2] = min elseif y > max then a[2] = max end
+    if z < min then a[3] = min elseif z > max then a[3] = max end
+  end
+  return a
+end
+
+function class.clamp_min(a, min, b)
+  if b then
+    local x = b[1]
+    local y = b[2]
+    local z = b[3]
+    if x < min then a[1] = min else a[1] = x end
+    if y < min then a[2] = min else a[2] = y end
+    if z < min then a[3] = min else a[3] = z end
+  else
+    if a[1] < min then a[1] = min end
+    if a[2] < min then a[2] = min end
+    if a[3] < min then a[3] = min end
+  end
+  return a
+end
+
+function class.clamp_max(a, max, b)
+  if b then
+    local x = b[1]
+    local y = b[2]
+    local z = b[3]
+    if x > max then a[1] = max else a[1] = x end
+    if y > max then a[2] = max else a[2] = y end
+    if z > max then a[3] = max else a[3] = z end
+  else
+    if a[1] > max then a[1] = max end
+    if a[2] > max then a[2] = max end
+    if a[3] > max then a[3] = max end
+  end
+  return a
+end
+
+function class.absolute(a, b)
+  if b then
+    local x = b[1]
+    local y = b[2]
+    local z = b[3]
+    if x < 0 then a[1] = -x else a[1] = x end
+    if y < 0 then a[2] = -y else a[2] = y end
+    if z < 0 then a[3] = -z else a[3] = z end
+  else
+    local x = a[1]
+    local y = a[2]
+    local z = a[3]
+    if x < 0 then a[1] = -x end
+    if y < 0 then a[2] = -y end
+    if z < 0 then a[3] = -z end
+  end
+  return a
+end
+
+function class.interpolate(a, b, c, d)
+  if d then
+    local beta = 1 - d
+    a[1] = beta * b[1] + d * c[1]
+    a[2] = beta * b[2] + d * c[2]
+    a[3] = beta * b[3] + d * c[3]
+  else
+    local beta = 1 - c
+    a[1] = beta * a[1] + c * b[1]
+    a[2] = beta * a[2] + c * b[2]
+    a[3] = beta * a[3] + c * b[3]
+  end
+  return a
+end
+
+function metatable.__index(a, key)
+  if key == "x" then
+    return a[1]
+  elseif key == "y" then
+    return a[2]
+  elseif key == "z" then
+    return a[3]
+  else
+    return class[key]
+  end
+end
+
+function metatable.__newindex(a, key, value)
+  if key == "x" then
+    rawset(a, 1, value)
+  elseif key == "y" then
+    rawset(a, 2, value)
+  elseif key == "z" then
+    rawset(a, 3, value)
+  else
+    rawset(a, key, value)
+  end
+end
+
+return setmetatable(class, {
+  __call = function (_, x, y, z)
+    return setmetatable(set({}, x, y, z), metatable)
+  end;
+})
---
+++ test/test_tuple.lua
@@ -0,0 +1,73 @@
+-- Copyright (C) 2018 Tomoyuki Fujimori <moyu@dromozoa.com>
+--
+-- This file is part of dromozoa-vecmath.
+--
+-- dromozoa-vecmath is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- dromozoa-vecmath is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with dromozoa-vecmath.  If not, see <http://www.gnu.org/licenses/>.
+
+local tuple3 = require "dromozoa.vecmath.tuple3"
+
+local verbose = os.getenv "VERBOSE" == "1"
+
+local t1 = tuple3()
+local t2 = tuple3(1, 2, 3)
+local t3 = tuple3 { 4, 5, 6 }
+
+assert(t1:equals { 0, 0, 0 })
+assert(t2:equals { 1, 2, 3 })
+assert(t3:equals { 4, 5, 6 })
+
+assert(t1 == { 0, 0, 0 })
+assert(t2 == { 1, 2, 3 })
+assert(t3 == { 4, 5, 6 })
+
+assert(t2:add(t3) == { 5, 7, 9 })
+assert(t1:add(t2, t3) == { 9, 12, 15 })
+
+assert(t2:sub(t3) == { 1, 2, 3 })
+assert(t1:sub(t3, t2) == { 3, 3, 3 })
+
+local t = tuple3()
+assert(t:negate { -1, 0, 1 } == { 1, 0, -1 })
+assert(t:negate() == { -1, 0, 1 })
+
+local t = tuple3()
+assert(t:clamp(1, 5, { 0, 3, 6 }) == { 1, 3, 5 })
+assert(t:clamp(2, 4) == { 2, 3, 4 })
+
+local t = tuple3()
+assert(t:clamp_min(1, { 0, 2, 4 }) == { 1, 2, 4 })
+assert(t:clamp_min(3) == { 3, 3, 4 })
+
+local t = tuple3()
+assert(t:clamp_max(3, { 0, 2, 4 }) == { 0, 2, 3 })
+assert(t:clamp_max(1) == { 0, 1, 1 })
+
+local t = tuple3()
+assert(t:absolute { -1, 0, 1 } == { 1, 0, 1 })
+assert(t:set( -1, 0, 1 ):absolute()  == { 1, 0, 1 })
+
+local t = tuple3(1, 2, 3)
+assert(t.x == 1)
+assert(t.y == 2)
+assert(t.z == 3)
+
+t.x = 2
+t.y = 3
+t.z = 4
+assert(t == { 2, 3, 4 })
+
+t[1] = 3
+t[2] = 4
+t[3] = 5
+assert(t == { 3, 4, 5 })
