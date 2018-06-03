bc291a7c87c0a47c47d48a81f3966d04f4e9c606
Tomoyuki Fujimori <moyu@dromozoa.com>
2018-06-04 08:34:47 +0900

    テストを修正。

--- test/test_tuple.lua
+++ test/test_tuple.lua
@@ -16,64 +16,101 @@
 -- along with dromozoa-vecmath.  If not, see <http://www.gnu.org/licenses/>.
 
 local tuple3 = require "dromozoa.vecmath.tuple3"
+local vector3 = require "dromozoa.vecmath.vector3"
 
 local verbose = os.getenv "VERBOSE" == "1"
 
-local t1 = tuple3()
-local t2 = tuple3(1, 2, 3)
-local t3 = tuple3 { 4, 5, 6 }
+local function test(n, class)
+  assert(class()        :equals {0,0,0,0})
+  assert(class(1,2,3,4) :equals {1,2,3,4})
+  assert(class{1,2,3,4} :equals {1,2,3,4})
 
-assert(t1:equals { 0, 0, 0 })
-assert(t2:equals { 1, 2, 3 })
-assert(t3:equals { 4, 5, 6 })
+  local t = class(1,2,3,4)
+  assert(t:set()        :equals {0,0,0,0})
+  assert(t:set(2,3,4,5) :equals {2,3,4,5})
+  assert(t:set{3,4,5,6} :equals {3,4,5,6})
 
-assert(t1:equals { 0, 0, 0 })
-assert(t2:equals { 1, 2, 3 })
-assert(t3:equals { 4, 5, 6 })
+  local t1 = class(1,2,3,4)
+  local t2 = class(2,3,4,5)
+  assert(t1:get(t2) == t1)
+  assert(t1:equals(t2))
 
-assert(t2:add(t3):equals { 5, 7, 9 })
-assert(t1:add(t2, t3):equals { 9, 12, 15 })
+  local t1 = class(1,2,3,4)
+  local t2 = class(2,3,4,5)
+  local t3 = class(3,4,5,6)
+  assert(t2:add(t3)     :equals {5,7,9,11})
+  assert(t1:add(t2, t3) :equals {8,11,14,17})
 
-assert(t2:sub(t3):equals { 1, 2, 3 })
-assert(t1:sub(t3, t2):equals { 3, 3, 3 })
+  local t1 = class(3,4,5,6)
+  local t2 = class(2,3,4,5)
+  local t3 = class(1,2,3,4)
+  assert(t1:sub(t2)     :equals {1,1,1,1})
+  assert(t1:sub(t2, t3) :equals {1,1,1,1})
 
-local t = tuple3()
-assert(t:negate { -1, 0, 1 }:equals { 1, 0, -1 })
-assert(t:negate():equals { -1, 0, 1 })
+  local t1 = class( 0,-1,-1,-1)
+  local t2 = class(-1, 0,-1,-1)
+  assert(t1:negate()   :equals {0,1,1,1})
+  assert(t1:negate(t2) :equals {1,0,1,1})
 
-local t = tuple3()
-assert(t:clamp(1, 5, { 0, 3, 6 }):equals { 1, 3, 5 })
-assert(t:clamp(2, 4):equals { 2, 3, 4 })
+  local t = class()
+  assert(t:clamp(1, 5, {0,6,3,3}) :equals {1,5,3,3})
+  assert(t:clamp(2, 4)            :equals {2,4,3,3})
 
-local t = tuple3()
-assert(t:clamp_min(1, { 0, 2, 4 }):equals { 1, 2, 4 })
-assert(t:clamp_min(3):equals { 3, 3, 4 })
+  local t = class()
+  assert(t:clamp_min(1, {0,6,3,3}) :equals {1,6,3,3})
+  assert(t:clamp_min(2)            :equals {2,6,3,3})
 
-local t = tuple3()
-assert(t:clamp_max(3, { 0, 2, 4 }):equals { 0, 2, 3 })
-assert(t:clamp_max(1):equals { 0, 1, 1 })
+  local t = class()
+  assert(t:clamp_max(5, {0,6,3,3}) :equals {0,5,3,3})
+  assert(t:clamp_max(4)            :equals {0,4,3,3})
 
-local t = tuple3()
-assert(t:absolute { -1, 0, 1 }:equals { 1, 0, 1 })
-assert(t:set( -1, 0, 1 ):absolute() :equals { 1, 0, 1 })
+  local t = class()
+  assert(t:absolute{2,-1,-1,-1} :equals {2,1,1,1})
+  local t = class(2,-1,-1,-1)
+  assert(t:absolute() :equals {2,1,1,1})
 
-local t = tuple3(1, 2, 3)
-assert(t.x == 1)
-assert(t.y == 2)
-assert(t.z == 3)
+  local t = class(1,2,3,4)
+  assert(t.x == 1)
+  assert(t.y == 2)
+  if n > 2 then
+    assert(t.z == 3)
+    if n > 3 then
+      assert(t.w == 4)
+    end
+  end
 
-t.x = 2
-t.y = 3
-t.z = 4
-assert(t:equals { 2, 3, 4 })
+  t.x = 2
+  t.y = 3
+  if n > 2 then
+    t.z = 4
+    if n > 3 then
+      t.w = 5
+    end
+  end
+  assert(t :equals {2,3,4,5})
 
-t[1] = 3
-t[2] = 4
-t[3] = 5
-assert(t:equals { 3, 4, 5 })
+  t[1] = 3
+  t[2] = 4
+  if n > 2 then
+    t[3] = 5
+    if n > 3 then
+      t[4] = 6
+    end
+  end
+  assert(t :equals {3,4,5,6})
 
-local s = tostring(t)
-if verbose then
-  print(s)
+  local s = tostring(t)
+  if verbose then
+    print(s)
+  end
+  if n == 2 then
+    assert(s == "(3,4)")
+  elseif n == 3 then
+    assert(s == "(3,4,5)")
+  elseif n == 4 then
+    assert(s == "(3,4,5,6)")
+  end
 end
-assert(s == "(3,4,5)")
+
+test(3, tuple3)
+test(3, vector3)
