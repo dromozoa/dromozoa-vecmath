b07e152b1337e233f7b42a61e43191c97e27d366
Tomoyuki Fujimori <moyu@dromozoa.com>
2018-06-22 17:02:09 +0900

    Bezier Clippingの実装を修正。

--- test/bezier_clipping.lua
+++ test/bezier_clipping.lua
@@ -28,15 +28,20 @@ local point2 = vecmath.point2
 local vector2 = vecmath.vector2
 local curve = vecmath.curve
 
-local function draw_fat_line(node, p1, p2, p3, p4)
+local function distance(v1, v2)
+  return v1.x * v2.y - v1.y * v2.x
+end
+
+local function draw_fat_line(node, P)
+  local p1, p2, p3, p4 = unpack(P)
   local v1 = vector2():sub(p4, p1):normalize()
   local v2 = vector2():sub(p2, p1)
   local v3 = vector2():sub(p3, p1)
 
   local d_min
   local d_max
-  local d1 = v1.x * v2.y - v1.y * v2.x
-  local d2 = v1.x * v3.y - v1.y * v3.x
+  local d1 = distance(v1, v2)
+  local d2 = distance(v1, v3)
   if d1 * d2 > 0 then
     d_min = math.min(0, d1, d2) * 3 / 4
     d_max = math.max(0, d1, d2) * 3 / 4
@@ -44,6 +49,9 @@ local function draw_fat_line(node, p1, p2, p3, p4)
     d_min = math.min(0, d1, d2) * 4 / 9
     d_max = math.max(0, d1, d2) * 4 / 9
   end
+
+  print("d_min", d_min)
+  print("d_max", d_max)
 
   local v2 = vector2(-v1.y, v1.x)
   local q1 = point2():scale_add(d_min, v2, p1)
@@ -65,9 +73,12 @@ local function draw_fat_line(node, p1, p2, p3, p4)
     fill = "none";
     stroke = "#F00";
   }
+
+  return d_min, d_max
 end
 
-local function draw_cubic_bezier(node, p1, p2, p3, p4)
+local function draw_cubic_bezier(node, P)
+  local p1, p2, p3, p4 = unpack(P)
   local d = path_data()
   d:M(p1.x, p1.y):L(p2.x, p2.y):L(p3.x, p3.y):L(p4.x, p4.y)
   node[#node + 1] = _"path" {
@@ -84,6 +95,87 @@ local function draw_cubic_bezier(node, p1, p2, p3, p4)
   }
 end
 
+local function draw_convex_full(node, P)
+  local p1, p2, p3, p4 = unpack(P)
+  node[#node + 1] = _"path" {
+    d = path_data():M(p1.x, p1.y):L(p2.x, p2.y):L(p3.x, p3.y):Z();
+    fill = "#00F";
+    ["fill-opacity"] = 0.125;
+  }
+  node[#node + 1] = _"path" {
+    d = path_data():M(p1.x, p1.y):L(p2.x, p2.y):L(p4.x, p4.y):Z();
+    fill = "#00F";
+    ["fill-opacity"] = 0.125;
+  }
+  node[#node + 1] = _"path" {
+    d = path_data():M(p1.x, p1.y):L(p3.x, p3.y):L(p4.x, p4.y):Z();
+    fill = "#00F";
+    ["fill-opacity"] = 0.125;
+  }
+  node[#node + 1] = _"path" {
+    d = path_data():M(p2.x, p2.y):L(p3.x, p3.y):L(p4.x, p4.y):Z();
+    fill = "#00F";
+    ["fill-opacity"] = 0.125;
+  }
+end
+
+local function draw_td(node, P, Q, d_min, d_max)
+  local p1, p2, p3, p4 = unpack(P)
+  local q1, q2, q3, q4 = unpack(Q)
+
+  local v = vector2():sub(p4, p1):normalize()
+  local d1 = distance(v, vector2():sub(q1, p1))
+  local d2 = distance(v, vector2():sub(q2, p1))
+  local d3 = distance(v, vector2():sub(q3, p1))
+  local d4 = distance(v, vector2():sub(q4, p1))
+
+  print(d1, d2, d3, d4)
+
+  node[#node + 1] = _"rect" {
+    x = 0;
+    y = d_min;
+    width = 320;
+    height = d_max - d_min;
+    fill = "#F00";
+    ["fill-opacity"] = 0.25;
+  }
+
+  local d = path_data()
+  d:M(0, d1):L(320/3, d2):L(640/3, d3):L(320, d4)
+  node[#node + 1] = _"path" {
+    d = d;
+    fill = "none";
+    stroke = "#999";
+  }
+
+  local a1 = point2(0,     d1)
+  local a2 = point2(320/3, d2)
+  local a3 = point2(640/3, d3)
+  local a4 = point2(320,   d4)
+  draw_convex_full(node, { a1, a2, a3, a4 })
+
+  local d = path_data()
+  d:M(0, d1):C(320/3, d2, 640/3, d3, 320, d4)
+  node[#node + 1] = _"path" {
+    d = d;
+    fill = "none";
+    stroke = "#333";
+  }
+
+  local d = path_data()
+  d:M(0, d1)
+  for i = 1, 1024 do
+    local t = i / 1024
+    local q = curve.cubic_bezier(q1, q2, q3, q4, t, point2())
+    d:L(320 * t, distance(v, vector2():sub(q, p1)))
+  end
+  node[#node + 1] = _"path" {
+    d = d;
+    fill = "none";
+    stroke = "#000";
+  }
+end
+
 local P = {
   point2(-240,    0);
   point2( -80,   80);
@@ -94,7 +186,7 @@ local P = {
 local Q = {
   point2(-50, -150);
   point2(-25,  200);
-  point2(150, -300);
+  point2(150, 300);
   point2(150,  150);
 }
 
@@ -102,26 +194,47 @@ local root = _"g" {
   transform = "translate(320, 320)";
 }
 
+local td_root = _"g" {
+  transform = "translate(640, 320)";
+}
+
 -- P,Q = Q,P
 
-draw_fat_line(root, unpack(P))
-draw_cubic_bezier(root, unpack(P))
-draw_cubic_bezier(root, unpack(Q))
+local d_min, d_max = draw_fat_line(root, P)
+draw_cubic_bezier(root, P)
+draw_cubic_bezier(root, Q)
+draw_convex_full(root, Q)
+draw_td(td_root, P, Q, d_min, d_max)
 
 local svg = _"svg" {
   xmlns = "http://www.w3.org/2000/svg";
   ["xmlns:xlink"] ="http://www.w3.org/1999/xlink";
   version = "1.1";
-  width = 640;
+  width = 960;
   height = 640;
   _"rect" {
     x = 0;
     y = 0;
     width = 640;
     height = 640;
-    fill = "#CCC";
+    fill = "none";
+    stroke = "#CCC";
+  };
+  _"rect" {
+    x = 640;
+    y = 0;
+    width = 320;
+    height = 640;
+    fill = "none";
+    stroke = "#CCC";
+  };
+  _"path" {
+    d = "M640,320 h320";
+    fill = "none";
+    stroke = "#F00";
   };
   root;
+  td_root;
 }
 
 local doc = xml_document(svg)
