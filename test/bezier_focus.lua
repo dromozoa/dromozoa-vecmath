e2d40fff92f19df130dc7c1306e60e9903914a9c
Tomoyuki Fujimori <moyu@dromozoa.com>
2018-06-28 17:21:42 +0900

    Bezier Focusのサンプルを修正。

--- test/bezier_focus.lua
+++ test/bezier_focus.lua
@@ -26,55 +26,10 @@ local unpack = table.unpack or unpack
 local _ = element
 local point2 = vecmath.point2
 local vector2 = vecmath.vector2
-local vector3 = vecmath.vector3
-local vector4 = vecmath.vector4
 local matrix2 = vecmath.matrix2
-local matrix3 = vecmath.matrix3
-local matrix4 = vecmath.matrix4
 local curve = vecmath.curve
 
-local N = 16
-local M = 64
-
-local function cubic_bezier(P, t)
-  local p1, p2, p3, p4 = unpack(P)
-  local u = 1 - t
-  local p = vector2()
-  p:scale_add(t * t * t, p4, p)
-  p:scale_add(3 * u * t * t, p3, p)
-  p:scale_add(3 * u * u * t, p2, p)
-  p:scale_add(u * u * u, p1, p)
-  return p
-end
-
-local function cubic_bezier_d(P, t)
-  local p1, p2, p3, p4 = unpack(P)
-  local u = 1 - t
-  local v = vector2()
-  v:scale_add(t * t, p4, v)
-  v:scale_add(3 * t * (u - 1/3), p3, v)
-  v:scale_add(-3 * (t - 1/3) * u, p2, v)
-  v:scale_add(-u * u, p1, v)
-  v:scale(3)
-  return v
-end
-
-local function distance(P, F, t, u)
-  if u then
-    local a = cubic_bezier_d(P, t)
-    local b = cubic_bezier(P, t):sub(cubic_bezier(F, u))
-    return a:dot(b)
-  else
-    local a = cubic_bezier_d(P, t)
-    local b = cubic_bezier(P, t):sub(F)
-    return a:dot(b)
-  end
-end
-
-local function rot90(v)
-  v:set(-v.y, v.x)
-  return v
-end
+local n = 16
 
 local function draw_cubic_bezier(node, P)
   local p1, p2, p3, p4 = unpack(P)
@@ -109,44 +64,16 @@ local function draw_cubic_bezier(node, P)
     stroke = "#333";
   }
 
-  local d1 = path_data()
-  local d2 = path_data()
-  for i = 0, N do
-    local t = i / N
-    local u = 1 - t
-
-    local p = point2()
-    p:scale_add(u * u * u, p1, p)
-    p:scale_add(3 * u * u * t, p2, p)
-    p:scale_add(3 * u * t * t, p3, p)
-    p:scale_add(t * t * t, p4, p)
-
-    local v = vector2()
-    v:scale_add(t * t, p4, v)
-    v:scale_add(3 * t * (u - 1/3), p3, v)
-    v:scale_add(- 3 * (t - 1/3) * u, p2, v)
-    v:scale_add(- u * u, p1, v)
-    v:scale(3)
-    v.x, v.y = -v.y, v.x
-    v:scale(c[1] * u + c[2] * t)
-
-    d1:M(p.x, p.y):L(p.x + v.x, p.y + v.y)
-    if i == 0 then
-      d2:M(p.x + v.x, p.y + v.y)
-    else
-      d2:L(p.x + v.x, p.y + v.y)
-    end
+  local d = path_data()
+  for i = 0, n do
+    local t = i / n
+    local p = curve.cubic_bezier(P, t, point2())
+    local f = curve.cubic_bezier(F, t, point2())
+    d:M(p.x, p.y):L(f.x, f.y)
   end
 
   node[#node + 1] = _"path" {
-    d = d1;
-    fill = "none";
-    stroke = "#F33";
-    ["stroke-opacity"] = 0.5;
-  }
-
-  node[#node + 1] = _"path" {
-    d = d2;
+    d = d;
     fill = "none";
     stroke = "#F33";
     ["stroke-opacity"] = 0.5;
@@ -155,60 +82,7 @@ local function draw_cubic_bezier(node, P)
   return F
 end
 
-local function draw_td_p(node, P, F)
-  local p1, p2, p3, p4 = unpack(P)
-
-  node[#node + 1] = _"path" {
-    d = path_data():M(0, 0):H(640);
-    fill = "none";
-    stroke = "#CCC";
-  }
-
-  local d = path_data()
-  local x = 640
-  local y = 1/320
-
-  local d0 = distance(P, F, 0)
-  local d1 = distance(P, F, 1)
-  d:M(0, d0 * y)
-  for i = 1, M do
-    local t = i / M
-    d:L(t * x, distance(P, F, t) * y)
-  end
-
-  node[#node + 1] = _"path" {
-    d = d;
-    fill = "none";
-    stroke = "#333";
-  }
-
-  local m = matrix4(
-     48/30000, -48/30000,  32/30000, -12/30000,
-    -58/30000, 118/30000, -92/30000,  37/30000,
-     37/30000, -92/30000, 118/30000, -58/30000,
-    -12/30000,  32/30000, -48/30000,  48/30000)
-  local v = vector4(
-    3125 * distance(P, F, 1/5) - 1024 * d0 -        d1,
-    3125 * distance(P, F, 2/5) -  243 * d0 -   32 * d1,
-    3125 * distance(P, F, 3/5) -   32 * d0 -  243 * d1,
-    3125 * distance(P, F, 4/5) -        d0 - 1024 * d1)
-  m:transform(v)
-
-  local data = { d0, v.x, v.y, v.z, v.w, d1 }
-  local d = path_data()
-  d:M(0, data[1] * y)
-  for i = 1, 5 do
-    d:L(i / 5 * x, data[i + 1] * y)
-  end
-
-  node[#node + 1] = _"path" {
-    d = d;
-    fill = "none";
-    stroke = "#F33";
-  }
-end
-
-local function draw_td_c(node, P, F)
+local function draw_td(node, P, F)
   local p1, p2, p3, p4 = unpack(P)
 
   node[#node + 1] = _"path" {
@@ -224,37 +98,17 @@ local function draw_td_c(node, P, F)
     local u = j / 3
     local d = path_data()
 
-    local d0 = distance(P, F, 0, u)
-    local d1 = distance(P, F, 1, u)
-    d:M(0, d0 * y)
-    for i = 1, M do
-      local t = i / M
-      d:L(t * x, distance(P, F, t, u) * y)
-    end
+    local Q = { point2(), point2(), point2(), point2(), point2(), point2() }
+    curve.to_quintic_bezier(function (t, q)
+      local a = curve.diff_cubic_bezier(P, t, vector2())
+      local b = curve.cubic_bezier(P, t, vector2()):sub(curve.cubic_bezier(F, u, vector2()))
+      return q:set(t, a:dot(b))
+    end, Q)
 
-    -- node[#node + 1] = _"path" {
-    --   d = d;
-    --   fill = "none";
-    --   stroke = "#333";
-    -- }
-
-    local m = matrix4(
-       48/30000, -48/30000,  32/30000, -12/30000,
-      -58/30000, 118/30000, -92/30000,  37/30000,
-       37/30000, -92/30000, 118/30000, -58/30000,
-      -12/30000,  32/30000, -48/30000,  48/30000)
-    local v = vector4(
-      3125 * distance(P, F, 1/5, u) - 1024 * d0 -        d1,
-      3125 * distance(P, F, 2/5, u) -  243 * d0 -   32 * d1,
-      3125 * distance(P, F, 3/5, u) -   32 * d0 -  243 * d1,
-      3125 * distance(P, F, 4/5, u) -        d0 - 1024 * d1)
-    m:transform(v)
-
-    local data = { d0, v.x, v.y, v.z, v.w, d1 }
     local d = path_data()
-    d:M(0, data[1] * y)
-    for i = 1, 5 do
-      d:L(i / 5 * x, data[i + 1] * y)
+    d:M(Q[1].x * x, Q[1].y * y)
+    for i = 2, 6 do
+      d:L(Q[i].x * x, Q[i].y * y)
     end
 
     node[#node + 1] = _"path" {
@@ -292,8 +146,8 @@ local td_root = _"g" {
 
 local Fp = draw_cubic_bezier(root, P)
 local Fq = draw_cubic_bezier(root, Q)
--- draw_td_c(td_root, P, Fq)
-draw_td_c(td_root, Q, Fp)
+-- draw_td(td_root, P, Fq)
+draw_td(td_root, Q, Fp)
 
 -- root[#root + 1] = _"circle" {
 --   cx = F.x;
