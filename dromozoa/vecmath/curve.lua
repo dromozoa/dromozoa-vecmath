4118f2fc16765e60c3d5f32799ce2978531b8fdc
Tomoyuki Fujimori <moyu@dromozoa.com>
2018-06-28 11:05:46 +0900

    リファクタリング。

--- dromozoa/vecmath/curve.lua
+++ dromozoa/vecmath/curve.lua
@@ -17,72 +17,72 @@
 
 local class = {}
 
-function class.linear_bezier(c, t, p)
-  return p:interpolate(c[1], c[2], t)
+function class.linear_bezier(p, t, q)
+  return q:interpolate(p[1], p[2], t)
 end
 
-function class.diff_linear_bezier(c, t, v)
-  return v:sub(c[2], c[1])
+function class.diff_linear_bezier(p, t, v)
+  return v:sub(p[2], p[1])
 end
 
-function class.quadratic_bezier(c, t, p)
+function class.quadratic_bezier(p, t, q)
   local u = 1 - t
-  p:scale    (u * u,     c[1])
-  p:scale_add(t * t,     c[3], p)
-  p:scale_add(2 * u * t, c[2], p)
-  return p
+  q:scale    (u * u,     p[1])
+  q:scale_add(t * t,     p[3], q)
+  q:scale_add(2 * u * t, p[2], q)
+  return q
 end
 
-function class.diff_quadratic_bezier(c, t, v)
+function class.diff_quadratic_bezier(p, t, v)
   local u = 1 - t
-  v:scale    (- 2 * u,     c[1])
-  v:scale_add(  2 * t,     c[3], v)
-  v:scale_add(  2 - 4 * t, c[2], v)
+  v:scale    (- 2 * u,     p[1])
+  v:scale_add(  2 * t,     p[3], v)
+  v:scale_add(  2 - 4 * t, p[2], v)
   return v
 end
 
-function class.cubic_bezier(c, t, p)
+function class.cubic_bezier(p, t, q)
   local u = 1 - t
-  local _3ut= 3 * u * t
-  p:scale    (u * u * u, c[1])
-  p:scale_add(t * t * t, c[4], p)
-  p:scale_add(u * _3ut,  c[2], p)
-  p:scale_add(t * _3ut,  c[3], p)
-  return p
+  local _3ut = 3 * u * t
+  q:scale    (u * u * u, p[1])
+  q:scale_add(t * t * t, p[4], q)
+  q:scale_add(u * _3ut,  p[2], q)
+  q:scale_add(t * _3ut,  p[3], q)
+  return q
 end
 
-function class.diff_cubic_bezier(c, t, v)
+function class.diff_cubic_bezier(p, t, v)
   local u = 1 - t
   local _9t = 9 * t
-  v:scale    (- 3 * u * u,     c[1])
-  v:scale_add(  3 * t * t,     c[4], v)
-  v:scale_add(  u * (3 - _9t), c[2], v)
-  v:scale_add(  t * (6 - _9t), c[3], v)
+  v:scale    (- 3 * u * u,     p[1])
+  v:scale_add(  3 * t * t,     p[4], v)
+  v:scale_add(  u * (3 - _9t), p[2], v)
+  v:scale_add(  t * (6 - _9t), p[3], v)
   return v
 end
 
-function class.catmull_rom(c, t, p)
+function class.catmull_rom(p, t, q)
   local u = 1 - t
   local _th = t / 2
   local _uh = u / 2
   local _ut = u * t
   local _mut = -_ut
   local _13ut = 1 + 3 * _ut
-  p:scale    (_uh * _mut,        c[1])
-  p:scale_add(_uh * (u + _13ut), c[2], p)
-  p:scale_add(_th * (t + _13ut), c[3], p)
-  p:scale_add(_th * _mut,        c[4], p)
-  return p
+  q:scale    (_uh * _mut,        p[1])
+  q:scale_add(_uh * (u + _13ut), p[2], q)
+  q:scale_add(_th * (t + _13ut), p[3], q)
+  q:scale_add(_th * _mut,        p[4], q)
+  return q
 end
 
-function class.catmull_rom_to_cubic_bezier(c, p1, p2)
-  local c2 = c[2]
-  local c3 = c[3]
-  p1:scale_add( 1/6, c3, c2)
-  p1:scale_add(-1/6, c[1], p1)
-  p2:scale_add(-1/6, c[4], c3)
-  p2:scale_add( 1/6, c2, p2)
-  return p1, p2
+function class.catmull_rom_to_cubic_bezier(p, q1, q2)
+  local p2 = p[2]
+  local p3 = p[3]
+  q1:scale_add( 1/6, p3, p2)
+  q1:scale_add(-1/6, p[1], q1)
+  q2:scale_add( 1/6, p2, p3)
+  q2:scale_add(-1/6, p[4], q2)
+  return q1, q2
 end
 
 return class
